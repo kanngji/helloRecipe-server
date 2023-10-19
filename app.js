45bbcf8ae1f9ce4fb4f74b021b20a6d5eaa00e3b
kangji <alleneffort1994@gmail.com>
2023-10-19 23:58:45 +0900

    mysql2 connect

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 4000; // server port number
+const db = require("./lib/db.js");
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+  connection.connect((err) => {
+    if (err) {
+      console.error("MySQL 연결 오류: " + err.stack);
+      return;
+    }
+    console.log("MySQL 연결 성공, ID " + connection.threadId);
+  });
+  connection.end((err) => {
+    if (err) {
+      console.error("MySQL 연결 종료 오류: " + err.stack);
+      return;
+    }
+    console.log("MySQL 연결 종료");
+  });
 });
 
 app.listen(port, () => {
