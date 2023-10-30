4cd2d378e879e75fbc82facb5f18f8fc40d1d98a
kangji <alleneffort1994@gmail.com>
2023-10-31 02:25:34 +0900

    login router

--- routes/user/login.js
+++ routes/user/login.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../../lib/db");
 
-router.get("/", (req, res) => {
-  res.send("Hello World");
+router.post("/", (req, res) => {
+  const { id, password } = req.body;
+  const query = "SELECT * FROM user WHERE user_id = ? AND user_password = ?";
+  db.query(query, [id, password], (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.status(200).json({ message: "Login successful" });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  });
 });
 
 module.exports = router;
