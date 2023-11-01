14e73aceece41c39df6d0771f7812af2eaff9563
kangji <alleneffort1994@gmail.com>
2023-11-02 02:43:57 +0900

    dotenv , jsonwebtoken 설치

--- routes/user/login.js
+++ routes/user/login.js
@@ -2,16 +2,31 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../lib/db");
 
+// jwt 토큰 생성
+const jwt = require("jsonwebtoken");
+
 router.post("/", (req, res) => {
   const { id, password } = req.body;
   const query = "SELECT * FROM user WHERE user_id = ? AND user_password = ?";
   db.query(query, [id, password], (err, results) => {
-    if (err) throw err;
+    if (err) {
+      res.status(500).json({ message: "Internal Server Error" });
+      return;
+    }
     if (results.length > 0) {
+      const user = {
+        id: results[0].user_id,
+      };
+      // JWT 토큰 생성 및 서명
+      const token = jwt.sign(user, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1h",
+      });
+
       res.status(200).json({
         message: "Login successful",
         user_id: id,
-        user_password: password,
+
+        token: token,
       });
     } else {
       res.status(401).json({ message: "Invalid credentials" });
